76132dedc100331c8bbacbe1c58e056c2bcd422b
Srijana Niraula <niraulasrijana267@gmail.com>
2026-09-03 18:56:41 +0530

    add local

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// mobile.menu
+function toggleMenu({
+    douument.queryselector(".nav-links")
+    .Classlist.toggle("active");
+})
+// dark mode
+function toggletheme(){
+ document.body.classlist.toggle("dark");   
+}
+// contact form
+function validateform(){
+    alert("form submittedsucessfully!");
+    return false;
+
+}
+// back to top
+function GoTo (){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+}
+// welcome message
+window.onloa= fuction(){
+    console.log("welcome to b2 world website");
+};
