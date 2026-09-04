8978dfe17fc71a01f3fcf120586af454ab6b8f1c
Srijana Niraula <niraulasrijana267@gmail.com>
2026-09-04 13:29:05 +0530

    add local

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # b2world1__website
+A responsive company websit built using HTML,CSS AND Javascript.
+
+## Features
+Responsive design
+Home page
+About page
+Services page
+Project page
+Contact page
+Navigation menu
+Contact form
+
+## Author
+Srijana Niraula
+
+## Technology used
+-HTML5
+-CSS
+-Javascript
+
+## Intership
+UI/UX Developer Intern-B2World
 
--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-
-// mobile.menu
-function toggleMenu({
-    douument.queryselector(".nav-links")
-    .Classlist.toggle("active");
-})
-// dark mode
-function toggletheme(){
- document.body.classlist.toggle("dark");   
-}
-// contact form
-function validateform(){
-    alert("form submittedsucessfully!");
-    return false;
-
-}
-// back to top
-function GoTo (){
-    window.scrollTo({
-        top:0,
-        behavior:"smooth"
-    });
-}
-// welcome message
-window.onloa= fuction(){
-    console.log("welcome to b2 world website");
+// Welcome Alert
+window. onloaD= fuction(){
+    console.log("welcome to B2World Website");
 };
+// Contact Form
+const form=
+document.querySelector("form");
+
+if(form){
+    form.addEventListener("submit",
+        function(e){
+            e.precventDefault();
+            alert("Thank you! your message has been set.");
+                form.reset();
+        } );
+}
+// Navigation click
+const  navLinks=
+document.querySelectorAll("nav a");
+
+navLinks.forEach(link=>{
+    link.addEachListener("click",
+        function(){
+            console.log("opening:"  +
+                this.textContent );
+        }
+    )
+})
